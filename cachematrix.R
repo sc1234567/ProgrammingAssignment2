eeb2fa07b5fd180eba124c0784215e44572cff0c
sc1234567 <seth.curl@gmail.com>
2016-12-17 19:13:11 -0800

    updated cachematrix.R for assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the inverse of a matrix
 
-## Write a short comment describing this function
+rm(list=ls())
+setwd('~/Desktop/Coursera/R/Week_3')
 
-makeCacheMatrix <- function(x = matrix()) {
+## Load MASS library to use ginv command as solve did not work
+library(MASS)
 
+## Creates a matrix object that can cache its inverse.
+
+makeCacheMatrix <- function(x = matrix()) {             # initialize x and m
+        m <- NULL
+        set <- function(y) {                            
+                  x <<- y                               # assigns input argmuent to x in parent environment
+                  m <<- NULL                            # assigns value of NULL to m in parent environment
+        }
+        get <- function() x                             # retrieves x from parent environment
+        setinv <- function(inverse) m <<- inverse
+        getinv <- function() m
+        list(set = set, get = get,
+              setinv = setinv,
+              getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by makeCacheMatrix above.
+## If the inverse has already been calulated and the matrix has not changed,
+## cacheSolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- ginv(data)
+        x$setinv(m)
+        m
 }
+
+x <- matrix(1:16, 4, 4)
+a <- makeCacheMatrix(x)
